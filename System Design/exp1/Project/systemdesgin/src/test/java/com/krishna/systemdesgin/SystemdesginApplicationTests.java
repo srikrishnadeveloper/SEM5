1b10909b31eb7215a0cba695b5b583706ff771e1
Srikrishna <srikrishnawebdeveloper@gmail.com>
2026-07-15 21:28:04 +0530

    after generating spring project for system desgin lab

---
+++ System Design/exp1/Project/systemdesgin/src/test/java/com/krishna/systemdesgin/SystemdesginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krishna.systemdesgin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemdesginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
