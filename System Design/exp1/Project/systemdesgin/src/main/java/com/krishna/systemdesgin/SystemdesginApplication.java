14b05812b851ff733a8cfb45bc94ad124eb6df20
Srikrishna <srikrishnawebdeveloper@gmail.com>
2026-07-15 21:28:04 +0530

    after generating spring project for system desgin lab

---
+++ System Design/exp1/Project/systemdesgin/src/main/java/com/krishna/systemdesgin/SystemdesginApplication.java
@@ -0,0 +1,13 @@
+package com.krishna.systemdesgin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemdesginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemdesginApplication.class, args);
+	}
+
+}
