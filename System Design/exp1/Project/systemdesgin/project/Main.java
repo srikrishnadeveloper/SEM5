8967cc9263dfd23a47e13bbf0bab513618b885a4
Srikrishna <srikrishnawebdeveloper@gmail.com>
2026-07-16 21:06:30 +0530

    first

---
+++ System Design/exp1/Project/systemdesgin/project/Main.java
@@ -0,0 +1,13 @@
+package com.systemdesign.lab;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("team 42 - system design lab");
+        System.out.println();
+        System.out.println("dinesh kumar j");
+        System.out.println("gopinath s");
+        System.out.println("harish r");
+        System.out.println("srikrishna o s");
+    }
+}
